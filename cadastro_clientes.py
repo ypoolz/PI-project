7e126a1b0a7cfb5c8cb883690431065f729c7226
ypoolz <snt.biel15@gmail.com>
2022-10-25 11:23:00 -0300

    att

--- cadastro_clientes.py
+++ cadastro_clientes.py
@@ -2,11 +2,13 @@ import tkinter as tk
 import sqlite3
 import pandas as pd
 
-
+# #Criando DB:
 # conexao = sqlite3.connect('clientes_cadastro.db')
 
+# #Criação do cursor:
 # c = conexao.cursor()
-#
+
+# #Criação da tabela no DB:
 # c.execute(''' CREATE TABLE clientes (
 #   nome text,
 #   sobrenome text,
@@ -14,16 +16,18 @@ import pandas as pd
 #   telefone text
 #    )
 # ''')
-#
+
+# #Commit as mudanças:
 # conexao.commit()
-#
+
+# #Fechar DB
 # conexao.close()
 
 def cadastrar_clientes():
     conexao = sqlite3.connect('clientes_cadastro.db')
-
     c = conexao.cursor()
 
+    # Inserir dados na tabela DB:
     c.execute(" INSERT INTO clientes VALUES (:nome, :sobrenome, :email, :telefone)",
               {
                   'nome': entry_nome.get(),
@@ -37,6 +41,7 @@ def cadastrar_clientes():
 
     conexao.close()
 
+    # Apagar os valores das caixas de entrada:
     entry_nome.delete(0, 'end')
     entry_sobrenome.delete(0, 'end')
     entry_email.delete(0, 'end')
@@ -45,23 +50,28 @@ def cadastrar_clientes():
 
 def exportar_clientes():
     conexao = sqlite3.connect('clientes_cadastro.db')
-
     c = conexao.cursor()
-
+    
+    # Inserir dados na tabela:
     c.execute(" SELECT *, oid FROM clientes")
     clientes_cadastrados = c.fetchall()
-    clientes_cadastrados = pd.DataFrame(clientes_cadastrados, columns=['nome', 'sobrenome', 'email', 'telefone', 'Id_banco'])
-
-    clientes_cadastrados.to_excel('banco_clientes.xlsx')
+    
+    # Criar arquivo de planilha do excel:
+    clientes_cadastrados = pd.DataFrame(clientes_cadastrados, 
+                                        columns=['Nome', 'Sobrenome', 'E-mail', 'Telefone', 'Id'])
+    print(clientes_cadastrados)
+    # clientes_cadastrados.to_csv('clientes_cadastro.csv') 
+    
     conexao.commit()
 
     conexao.close()
 
+# Criação da janela:
 
 janela = tk.Tk()
 janela.title('Cadastro de clientes')
 
-# labels:
+# Nome das entradas:
 
 label_nome = tk.Label(janela, text='Nome')
 label_nome.grid(row=0, column=0, padx=10, pady=10)
@@ -75,26 +85,28 @@ label_email.grid(row=2, column=0, padx=10, pady=10)
 label_telefone = tk.Label(janela, text='Telefone')
 label_telefone.grid(row=3, column=0, padx=10, pady=10)
 
-# Entrys:
+# Caixas de entradas:
 
-entry_nome = tk.Entry(janela, text='Nome', width=30)
+entry_nome = tk.Entry(janela, width=30)
 entry_nome.grid(row=0, column=1, padx=10, pady=10)
 
-entry_sobrenome = tk.Entry(janela, text='Sobrenome', width=30)
+entry_sobrenome = tk.Entry(janela, width=30)
 entry_sobrenome.grid(row=1, column=1, padx=10, pady=10)
 
-entry_email = tk.Entry(janela, text='E-mail', width=30)
+entry_email = tk.Entry(janela, width=30)
 entry_email.grid(row=2, column=1, padx=10, pady=10)
 
-entry_telefone = tk.Entry(janela, text='Telefone', width=30)
+entry_telefone = tk.Entry(janela, width=30)
 entry_telefone.grid(row=3, column=1, padx=10, pady=10)
 
-# Botões:
+# Botões
 
+# Cadastrar:
 botao_cadastrar = tk.Button(janela, text='Cadastrar Clientes', command=cadastrar_clientes)
 botao_cadastrar.grid(row=4, column=0, padx=10, pady=10, columnspan=2, ipadx=80)
 
-entry_expotar = tk.Button(janela, text='Exportar Excel', command=exportar_clientes)
+# Exportar
+entry_expotar = tk.Button(janela, text='Ver informações', command=exportar_clientes)
 entry_expotar.grid(row=5, column=0, padx=10, pady=10, columnspan=2, ipadx=80)
 
 janela.mainloop()
